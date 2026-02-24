c52f1235776c98191cd27ce47c1228bea3a0a11e
solonovamax <solonovamax@12oclockpoint.com>
2026-02-24 16:12:18 -0500

    make nodes composable
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- include/sensor_filters/FilterChainNode.hpp
+++ include/sensor_filters/FilterChainNode.hpp
@@ -15,19 +15,28 @@ namespace sensor_filters {
     template <typename T, typename Base = FilterChainBase<T>>
     class FilterChainNode : public rclcpp::Node, public Base {
     public:
-        explicit FilterChainNode(const std::string& name) : Node(name), Base() {}
+        explicit FilterChainNode(const std::string& name, const rclcpp::NodeOptions& options) : Node(name, options), Base() {
+            this->initFilters(
+                name, this->node, false,
+                this->declare_parameter("input_queue_size", 10),
+                this->declare_parameter("output_queue_size", 10)
+            );
+        }
+
+        explicit FilterChainNode(const std::string& name) : Node(name), Base() {
+            this->initFilters(
+                name, this->node, false,
+                this->declare_parameter("input_queue_size", 10),
+                this->declare_parameter("output_queue_size", 10)
+            );
+        }
     };
 
 
     template <typename T, typename Base = FilterChainBase<T>>
-    void spinFilterChain(const std::string& name, const int argc, char** argv) {
+    void spinFilterChain(const int argc, char** argv) {
         rclcpp::init(argc, argv);
-        const auto node = std::make_shared<FilterChainNode<T, Base>>(name);
-        node->initFilters(
-            name, node, false,
-            node->declare_parameter("input_queue_size", 10),
-            node->declare_parameter("output_queue_size", 10)
-        );
+        const auto node = std::make_shared<FilterChainNode<T, Base>>();
         rclcpp::spin(node);
     }
 }
