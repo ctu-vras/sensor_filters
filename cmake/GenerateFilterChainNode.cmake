615f6ab9ba0807b4790d2ccbb64e65f98655c7c8
solonovamax <solonovamax@12oclockpoint.com>
2026-02-24 16:13:34 -0500

    cleanup GenerateFilterChainNode.cmake
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- cmake/GenerateFilterChainNode.cmake
+++ cmake/GenerateFilterChainNode.cmake
@@ -1,31 +1,12 @@
-# ${MESSAGE_TYPE} = sensor_msgs::msg::CompressedImage
-# ${NODE_NAME} = image_filter_chain
-
-#string(REGEX REPLACE "(_|^)([a-zA-Z])" "\\U$1\\E" FILE_NAME ${NODE_NAME})
-#string(APPEND FILE_NAME "FilterChainNode.cpp")
-#string(REPLACE "::" "/" MESSAGE_HEADER ${MESSAGE_TYPE})
-#string(APPEND MESSAGE_HEADER ".hpp")
-
 set(MESSAGE_TYPE "sensor_msgs::msg::${MESSAGE_TYPE}")
 set(MESSAGE_HEADER "sensor_msgs/msg/${HEADER_NAME}.hpp")
 
-message(MESSAGE_TYPE="${MESSAGE_TYPE}")
-message(HEADER_NAME="${HEADER_NAME}")
-message(MESSAGE_HEADER="${MESSAGE_HEADER}")
 if (BASE_FILTER)
     set(TEMPLATE_ARGS "${MESSAGE_TYPE}, sensor_filters::${BASE_FILTER}")
-    # string(REPLACE "::" "/" "${BASE_FILTER}")
     set(BASE_INCLUDE "#include <sensor_filters/${BASE_FILTER}.hpp>")
 else ()
     set(TEMPLATE_ARGS "${MESSAGE_TYPE}")
     set(BASE_INCLUDE "")
 endif ()
-message(NODE_NAME="${NODE_NAME}")
-message(BASE_FILTER="${BASE_FILTER}")
-message(TEMPLATE_ARGS="${TEMPLATE_ARGS}")
 
-# Configure the header file
 configure_file(${PROJECT_SOURCE_DIR}/cmake/FilterChainNode.cpp.in ${OUTPUT_FILE} @ONLY)
-
-file(READ ${OUTPUT_FILE} CONTENTS)
-message(CONTENTS=${CONTENTS})
