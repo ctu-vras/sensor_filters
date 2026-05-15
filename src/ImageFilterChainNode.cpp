37985b946e6d1355870b9e55ef6af6134dfc5306
Martin Pecka <peckama2@fel.cvut.cz>
2026-05-15 15:12:00 +0200

    Fixed image filter chain.

--- src/ImageFilterChainNode.cpp
+++ src/ImageFilterChainNode.cpp
@@ -7,27 +7,36 @@
 #include <image_transport/image_transport.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
+#include "node_helper.hpp"
+
 namespace sensor_filters {
     class ImageFilterChainNode : public FilterChainNode<sensor_msgs::msg::Image> {
     public:
-        explicit ImageFilterChainNode(const rclcpp::NodeOptions& options) : FilterChainNode("sensor_msgs::msg::Image", "Image_filter_chain", options) {
-            this->it = std::make_unique<image_transport::ImageTransport>(this->shared_from_this());
+        explicit ImageFilterChainNode(const rclcpp::NodeOptions& options) : FilterChainNode("sensor_msgs::msg::Image", "image_filter_chain", options) {
+            this->nodePtr = get_node_shared_ptr_from_raw_ptr(this);
+            // image_transport does not declare the parameter
+            this->declare_parameter("image_transport", "raw");
+            this->it = std::make_unique<image_transport::ImageTransport>(this->nodePtr);
             ImageFilterChainNode::configure();
         }
 
-        [[deprecated]] explicit ImageFilterChainNode() : FilterChainNode("sensor_msgs::msg::Image", "Image_filter_chain") {
-            this->it = std::make_unique<image_transport::ImageTransport>(this->shared_from_this());
+        [[deprecated]] explicit ImageFilterChainNode() : FilterChainNode("sensor_msgs::msg::Image", "image_filter_chain") {
+            this->nodePtr = get_node_shared_ptr_from_raw_ptr(this);
+            // image_transport does not declare the parameter
+            this->declare_parameter("image_transport", "raw");
+            this->it = std::make_unique<image_transport::ImageTransport>(this->nodePtr);
             ImageFilterChainNode::configure();
         }
 
     protected:
         void advertise() override {
-            this->itPublisher = this->it->advertise("output", this->outputQueueSize);
+            this->itPublisher = this->it->advertise(
+                this->get_node_topics_interface()->resolve_topic_name("output"), this->outputQueueSize);
         }
 
         void subscribe() override {
             this->itSubscriber = this->it->subscribe(
-                "input", this->inputQueueSize,
+                this->get_node_topics_interface()->resolve_topic_name("input"), this->inputQueueSize,
                 [this](const sensor_msgs::msg::Image::ConstSharedPtr& msg) {
                     this->callbackShared(msg);
                 }
@@ -39,6 +48,7 @@ namespace sensor_filters {
         }
 
     private:
+        rclcpp::Node::SharedPtr nodePtr;
         std::unique_ptr<image_transport::ImageTransport> it;
         image_transport::Publisher itPublisher;
         image_transport::Subscriber itSubscriber;
